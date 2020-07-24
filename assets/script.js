bcecbe84947ab276204a5dfbd04cf724bfc68ab3
jessef <1319957+jfrausto@users.noreply.github.com>
2020-07-24 01:20:00 -0700

    made welcome page with style and button

---
+++ assets/script.js
@@ -0,0 +1,10 @@
+var scoreDisplay = document.querySelector("#custom-score");
+var timerDisplay = document.querySelector("#custom-timer");
+var score = 0;
+var currentTime = 60; //    start with a
+
+scoreDisplay.hidden = true; //  hide score and timer areas
+timerDisplay.hidden = true;
+
+// scoreDisplay.hidden = false; //  hide score and timer areas
+// timerDisplay.hidden = false;
