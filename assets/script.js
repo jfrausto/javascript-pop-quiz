16b67c9eb55b510290640c0e66e529608c8ac3a6
jessef <1319957+jfrausto@users.noreply.github.com>
2020-07-24 19:40:06 -0700

    created html template, element references

--- assets/script.js
+++ assets/script.js
@@ -1,10 +1,21 @@
+// element selectors
 var scoreDisplay = document.querySelector("#custom-score");
 var timerDisplay = document.querySelector("#custom-timer");
-var score = 0;
-var currentTime = 60; //    start with a
+var titleDisplay = document.querySelector("#custom-header");
+var questionDisplay = document.querySelector("#question-display");
+var quizQuestion = document.querySelector("#quiz-question");
+var answerOne = document.querySelector("#answer1");
+var answerTwo = document.querySelector("#answer2");
+var answerThree = document.querySelector("#answer3");
+var answerFour = document.querySelector("#answer4");
 
-scoreDisplay.hidden = true; //  hide score and timer areas
+var score = 0;
+var currentTime = 60; //    start with a minute on the clock
+
+scoreDisplay.hidden = true; //  hide score, timer, and q&a areas
 timerDisplay.hidden = true;
+questionDisplay.hidden = true;
 
 // scoreDisplay.hidden = false; //  hide score and timer areas
 // timerDisplay.hidden = false;
+// questionDisplay.hidden = false;
