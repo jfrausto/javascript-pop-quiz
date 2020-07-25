7887ca049725acff4474f17d6345b9c2a95472d5
jessef <1319957+jfrausto@users.noreply.github.com>
2020-07-24 20:02:28 -0700

    created start quiz event, hide portions of layout

--- assets/script.js
+++ assets/script.js
@@ -2,6 +2,7 @@
 var scoreDisplay = document.querySelector("#custom-score");
 var timerDisplay = document.querySelector("#custom-timer");
 var titleDisplay = document.querySelector("#custom-header");
+var startButton = document.querySelector("#start-button");
 var questionDisplay = document.querySelector("#question-display");
 var quizQuestion = document.querySelector("#quiz-question");
 var answerOne = document.querySelector("#answer1");
@@ -9,9 +10,12 @@ var answerTwo = document.querySelector("#answer2");
 var answerThree = document.querySelector("#answer3");
 var answerFour = document.querySelector("#answer4");
 
-var score = 0;
 var currentTime = 60; //    start with a minute on the clock
+var score;
 
+// var quizQuestions = [
+
+// ];
 scoreDisplay.hidden = true; //  hide score, timer, and q&a areas
 timerDisplay.hidden = true;
 questionDisplay.hidden = true;
@@ -19,3 +23,13 @@ questionDisplay.hidden = true;
 // scoreDisplay.hidden = false; //  hide score and timer areas
 // timerDisplay.hidden = false;
 // questionDisplay.hidden = false;
+function startQuiz() {
+  titleDisplay.hidden = true;
+  startButton.hidden = true;
+  score = 0;
+  questionDisplay.hidden = false;
+  scoreDisplay.hidden = false;
+  // timerDisplay.hidden = false; // could be put in function instead
+}
+
+startButton.addEventListener("click", startQuiz);
