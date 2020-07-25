736c1729fcb0a7759abcb80ca431ec31c57b1fe5
jessef <1319957+jfrausto@users.noreply.github.com>
2020-07-24 23:45:35 -0700

    populated first question correctly in the html layout

--- assets/script.js
+++ assets/script.js
@@ -11,11 +11,27 @@ var answerThree = document.querySelector("#answer3");
 var answerFour = document.querySelector("#answer4");
 
 var currentTime = 60; //    start with a minute on the clock
-var score;
+var score = 0;
+var currentQuestion = 0; // keep adding to it to access others in the quiz Index
 
-// var quizQuestions = [
+var dataTypeQ = {
+  question: "Which of the following data types is NOT supported in JavaScript?",
+  answers: ["boolean", "number", "bit", "string"],
+  correctAnswer: "bit",
+};
+var selectHeaderQ = {
+  question: "Which is a valid way to concatenate an empty string? result = '';",
+  answers: [
+    "result = result + someOtherString;",
+    "result += someOtherString;",
+    "result = result.concat(someOtherString);",
+    "All of the above",
+  ],
+  correctAnswer: "All of the above",
+};
 
-// ];
+var quizQuestionsArray = [dataTypeQ, selectHeaderQ];
+console.log(quizQuestionsArray[1]);
 scoreDisplay.hidden = true; //  hide score, timer, and q&a areas
 timerDisplay.hidden = true;
 questionDisplay.hidden = true;
@@ -34,6 +50,7 @@ function startQuiz(event) {
   timerDisplay.hidden = false; // could be put in function instead
 
   setTime();
+  renderQuestion();
 }
 
 function setTime() {
@@ -46,6 +63,14 @@ function setTime() {
       // "YOU LOSE" FUNCTION,CONDITION,OR ACTION HERE
     }
   }, 1000);
+}
+
+function renderQuestion() {
+  quizQuestion.textContent = quizQuestionsArray[currentQuestion].question;
+  answerOne.textContent = quizQuestionsArray[currentQuestion].answers[0];
+  answerTwo.textContent = quizQuestionsArray[currentQuestion].answers[1];
+  answerThree.textContent = quizQuestionsArray[currentQuestion].answers[2];
+  answerFour.textContent = quizQuestionsArray[currentQuestion].answers[3];
 }
 
 startButton.addEventListener("click", startQuiz);
